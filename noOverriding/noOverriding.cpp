0064136931a2b94e793d65b7da03cc419f31c682
wawanpi <muhaguskurniawan@gmail.com>
2023-05-29 17:39:00 +0700

    done

--- noOverriding/noOverriding.cpp
+++ noOverriding/noOverriding.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class baseClass {
+public:
+	virtual void perkenalan() final {
+		cout << "Hallo saya Function dari base class";
+	}
+};
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << "Hallo saya Function dari derived Class";
+	}
+};
+int main() {
+	derivedClass a;
+	a.perkenalan();
+
+	return 0; 
 }
