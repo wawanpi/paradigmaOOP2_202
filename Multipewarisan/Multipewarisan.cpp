29dda0925867bda8cfbf75e31cbf1ffaaf8e2a09
wawanpi <muhaguskurniawan@gmail.com>
2023-05-29 15:04:08 +0700

    8

--- Multipewarisan/Multipewarisan.cpp
+++ Multipewarisan/Multipewarisan.cpp
@@ -30,5 +30,12 @@ public:
 
 class pelajar : private manusia, public orang {
 public:
+	string sekolah;
 
+	pelajar(string pNama, string pJenisKelamin, string pSekolah) :
+		orang(pNama),
+		manusia(pJenisKelamin),
+		sekolah(pSekolah) {
+		cout << "Pelajar dibuat\n" << endl;
+	}
 };
