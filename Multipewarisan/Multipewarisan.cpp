395d05c377deb159d63c6b87aa63b4c9e42aeb32
wawanpi <muhaguskurniawan@gmail.com>
2023-05-29 14:50:04 +0700

    membuat distructor

--- Multipewarisan/Multipewarisan.cpp
+++ Multipewarisan/Multipewarisan.cpp
@@ -4,5 +4,13 @@ using namespace std;
 
 class orang {
 public:
-	string nama
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+	~orang(){
+		cout << "orang dihapus\n" << endl;
+		}
 };
